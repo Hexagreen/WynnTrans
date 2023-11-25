48430a6b08e98cad099046abdfcdc173783154ab
Hexagreen <greentree329@naver.com>
2023-11-25 20:23:44 +0900

    Sign translation complete

--- src/main/java/net/hexagreen/wynntrans/mixin/MixinSignBlockEntity.java
+++ src/main/java/net/hexagreen/wynntrans/mixin/MixinSignBlockEntity.java
@@ -1,15 +1,10 @@
 package net.hexagreen.wynntrans.mixin;
 
 import net.hexagreen.wynntrans.WynnTrans;
-import net.hexagreen.wynntrans.sign.WynnSign;
 import net.minecraft.block.entity.SignBlockEntity;
-import net.minecraft.block.entity.SignText;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
